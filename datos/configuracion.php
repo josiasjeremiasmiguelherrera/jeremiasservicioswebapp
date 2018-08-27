9d5070cd92027d1e9533ab726b0905a127303578
josiasjeremiasmiguelherrera <42750053+josiasjeremiasmiguelherrera@users.noreply.github.com>
2018-08-27 13:49:01 -0500

    Add files via upload

---
+++ datos/configuracion.php
@@ -0,0 +1,25 @@
+<?php
+
+// Notificar todos los errores excepto E_NOTICE
+error_reporting(E_ALL ^ E_NOTICE);
+
+
+/*
+define("SERVIDOR_BD", "ec2-54-83-59-120.compute-1.amazonaws.com");
+define("USUARIO_BD", "tqbiekovboawgj");
+define("CLAVE_BD", "4d8731a0e5caaa2d67aad9a346b49990a3924c3f3d390a72b7ac0672e47bfdfc");
+define("PUERTO_BD", "5432");
+define("NOMBRE_BD", "dsm6mcob1i1me");
+*/
+
+
+
+define("SERVIDOR_BD", "localhost");
+define("USUARIO_BD", "postgres");
+define("CLAVE_BD", "USAT2018");
+define("PUERTO_BD", "5432");
+define("NOMBRE_BD", "bdserviciosweb");
+
+
+
+ 

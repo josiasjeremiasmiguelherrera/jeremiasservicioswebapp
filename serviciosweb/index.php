b3267ed67ee7599c037bfdd3d06efd5e4ef1ed89
josiasjeremiasmiguelherrera <42750053+josiasjeremiasmiguelherrera@users.noreply.github.com>
2018-08-27 14:08:05 -0500

    Update index.php

--- serviciosweb/index.php
+++ serviciosweb/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Servicios web</title>
+        <title>Servicios web JEREMIAS</title>
     </head>
     <body>
         <h2>Servicios web</h2>

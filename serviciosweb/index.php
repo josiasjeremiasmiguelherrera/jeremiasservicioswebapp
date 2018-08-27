ecaee057d717d0f1526f13288da7ba87b0b28000
josiasjeremiasmiguelherrera <42750053+josiasjeremiasmiguelherrera@users.noreply.github.com>
2018-08-27 14:07:25 -0500

    Add files via upload

---
+++ serviciosweb/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Servicios web</title>
+    </head>
+    <body>
+        <h2>Servicios web</h2>
+    </body>
+</html>
